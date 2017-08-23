1b2ea80b92a5a1eff0a276a1e779fcf2499f331f
Alexandre Abita <xouabita@gmail.com>
2017-08-23 12:26:01 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const isHeroku = require("is-heroku")
+
+module.exports = (config, {target, dev}, webpack) => {
+  const isDefinePlugin = plugin => plugin.constructor.name === "DefinePlugin"
+  const indexDefinePlugin = config.plugins.findIndex(isDefinePlugin)
+
+  const {definitions} = config.plugins[indexDefinePlugin]
+  const newDefs = {}
+
+  const writeDefs = ([key, val]) => (newDefs[`process.env.${key}`] = val)
+
+  Object.entries(definitions["process.env"]).forEach(writeDefs)
+
+  if (isHeroku) {
+    delete newDefs["process.env.PORT"]
+    newDefs["process.env.RAZZLE_PUBLIC_DIR"] = '"/app/build/public"'
+  }
+
+  config.plugins[indexDefinePlugin] = new webpack.DefinePlugin(newDefs)
+
+  return config
+}
